fbc6093a56208fe1780258786b18b2b838cfed59
Alexander Ostrovsky <thealeos@gmail.com>
2022-12-01 17:59:10 +0400

    Handle hardware permissions on the device

--- AgoraSandbox/LiveStream.swift
+++ AgoraSandbox/LiveStream.swift
@@ -7,3 +7,44 @@
 
 import AgoraRtcKit
 import Foundation
+
+@MainActor class LiveStream {
+    func checkForPermissions() -> Bool {
+        var hasPermissions = false
+        
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized: hasPermissions = true
+        default: hasPermissions = requestCameraAccess()
+        }
+        // Break out, because camera permissions have been denied or restricted.
+        if !hasPermissions { return false }
+        switch AVCaptureDevice.authorizationStatus(for: .audio) {
+        case .authorized: hasPermissions = true
+        default: hasPermissions = requestAudioAccess()
+        }
+        return hasPermissions
+    }
+    
+    func requestCameraAccess() -> Bool {
+        var hasCameraPermission = false
+        let semaphore = DispatchSemaphore(value: 0)
+        AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+            hasCameraPermission = granted
+            semaphore.signal()
+        })
+        semaphore.wait()
+        return hasCameraPermission
+    }
+    
+    func requestAudioAccess() -> Bool {
+        var hasAudioPermission = false
+        let semaphore = DispatchSemaphore(value: 0)
+        AVCaptureDevice.requestAccess(for: .audio, completionHandler: { granted in
+            hasAudioPermission = granted
+            semaphore.signal()
+        })
+        semaphore.wait()
+        return hasAudioPermission
+    }
+    
+}
