f810a0a323ef0f1f1db96cb489ff4380d102d3cc
Alexander Ostrovsky <thealeos@gmail.com>
2022-12-01 17:18:16 +0400

    Add an empty project

---
+++ AgoraSandbox/AgoraSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgoraSandboxApp.swift
+//  AgoraSandbox
+//
+//  Created by Alexander Ostrovsky on 01.12.22.
+//
+
+import SwiftUI
+
+@main
+struct AgoraSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
